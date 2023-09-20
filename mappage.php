cfbdfc6cd1bf53d12d06c0a3d0f58c21f1a754ce
NikitasPapag <127061290+NikitasPapag@users.noreply.github.com>
2023-09-20 03:48:35 +0300

    Update mappage.php

--- mappage.php
+++ mappage.php
@@ -6,10 +6,12 @@
 		<?php
 			if(isset($_SESSION["userid"]))
 			{
-		?>	
+		
 			<li><a href="profile.php"><?php echo $_SESSION["useruid"]; ?></a></li>
 			<li><a href="includes/logout.inc.php" class="header-login-a">LOGOUT</a></li>
-
+			}
+		?>	
+		
 	<section class="map-page">
 		<h1>Χάρτης Καταστημάτων</h1>
 		<p>Επιλέξτε το κατάστημα που επιθυμείτε από τον χάρτη, για να ενημερωθείτε για νέες προσφορές.</p>
